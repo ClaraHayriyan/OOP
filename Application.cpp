b978e51d0df87ba3e5af72dad5a74e0df1104dea
Tigran Khachakranc <tigrankhachakranc@users.noreply.github.com>
2023-12-09 13:55:17 +0400

    Comments

--- Application.cpp
+++ Application.cpp
@@ -22,7 +22,7 @@ bool& Application::getQuit() {
 void Application::exec() {
     while(!quit_) {
         try {
-           controller_.run(); 
+           controller_.run(); // TK: This way you can't make your app UI based, pass input stream to the run as an argument istead of constructor 
         }
         catch (std::runtime_error err) {
             std::cout << err.what() << std::endl;
