ff9e420f7efb746b5564bb962dc2f3a3ca1eb52f
Tigran Khachakranc <tigrankhachakranc@users.noreply.github.com>
2023-12-09 14:23:43 +0400

    Commnts

--- CommandController/Controller/CommandController.cpp
+++ CommandController/Controller/CommandController.cpp
@@ -6,6 +6,7 @@ CommandController::CommandController(std::istream& input)
 }
 
 void CommandController::run() {
+    // TK: get you input nline here and pass istrim to parser, you do not need to keep parser as a memeber, you can simply create parser instance as local variable right here
     CommandPtr commandPtr = parser_.parseCommand();
     commandPtr->execute();
 }
